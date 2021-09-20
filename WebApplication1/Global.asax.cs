f81902763259746ec829eda93eb353450e1fab93
jzd-1999 <1771887904@qq.com>
2021-09-20 14:28:14 +0800

    第二次修改

--- WebApplication1/Global.asax.cs
+++ WebApplication1/Global.asax.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
+
 namespace WebApplication1
 {
     public class MvcApplication : System.Web.HttpApplication
@@ -13,6 +14,7 @@ namespace WebApplication1
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+           // GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
